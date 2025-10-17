018b1567c9e9fcdefb28ec6d0ff51237d0e225a8
NguyenTuan2005 <nguyenvoquoctuan2104@gmail.com>
2025-10-17 22:19:17 +0700

    create layout and service

--- src/main/java/com/da17/my_pet_health/controller/ServiceController.java
+++ src/main/java/com/da17/my_pet_health/controller/ServiceController.java
@@ -1,16 +1,14 @@
 package com.da17.my_pet_health.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class ServiceController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Welcome to the Home Page!");
-        return "index";
+    @GetMapping("/dich-vu")
+    public String getService(Model model) {
+        return "service/service";
     }
 }
